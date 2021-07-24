23d0e926ca2c1c28656c9232fa2388fe51ed52ae
Michael <michael@tricht.dev>
2021-07-24 14:15:35 +0200

    Swap alt and lower.

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
+ *                   | LOWER| LGUI | LAlt | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
  *                   |      |      |      |/       /         \      \ |      |      |        |
  *                   `----------------------------'           '------''----------------------'
  */
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                             KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_DELETE
+                             LOWER, KC_LGUI,KC_LALT , KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_DELETE
 ),
 /* WoW
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   7   |    |QWERTY |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | 6    | LOWER| /Space  /       \Enter \  |RAISE |BackSP| DELETE |
+ *                   | LOWER| 6    | LAlt | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
  *                   |      |      |      |/       /         \      \ |      |      |        |
  *                   `----------------------------'           '------''----------------------'
  */
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_7,  QWERTY,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                             KC_LALT, KC_6,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_DELETE
+                             LOWER, KC_6,KC_LALT , KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_DELETE
    ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |  =   |  +   |  |   |   \  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
+ *                   | LOWER| LGUI | LAlt | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
  *                   |      |      |      |/       /         \      \ |      |      |        |
  *                   `----------------------------'           '------''----------------------'
  */
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
+ *                   | LOWER| LGUI | LAlt | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
  *                   |      |      |      |/       /         \      \ |      |      |        |
  *                   `----------------------------'           '------''----------------------'
  */
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| PREV  |    | NEXT  |------+------+------+------+------+---------|
  * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL-    |
  * `-----------------------------------------/       /     \      \--------------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
+ *                   | LOWER| LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| DELETE |
  *                   |      |      |      |/       /         \      \ |      |      |        |
  *                   `----------------------------'           '------''----------------------'
  */
